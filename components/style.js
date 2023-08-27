9984b76bf9ebc47d0a3b03946c87cb858cc91c77
Glatus <janipudas1@gmail.com>
2023-08-27 13:52:02 +0300

    Lista oli broken 🥲

--- components/style.js
+++ components/style.js
@@ -5,6 +5,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#232622',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
     paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
