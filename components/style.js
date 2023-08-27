3901aa5321c390ea19abee64a48861993921a2a4
Glatus <janipudas1@gmail.com>
2023-08-27 16:15:35 +0300

    Kuvat skaalaa oikein 🦉

--- components/style.js
+++ components/style.js
@@ -1,4 +1,7 @@
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   // App
@@ -64,8 +67,9 @@ const styles = StyleSheet.create({
   },
   // Single
   Simage: {
-    height: 500,
-    width: '100%',
+    width: screenWidth,
+    height: screenHeight,
+    resizeMode: 'contain',
   },
 });
 export default styles;
