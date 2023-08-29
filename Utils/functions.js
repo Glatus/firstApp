87e344330d006aec12f05345cfc3cc759746b313
Glatus <janipudas1@gmail.com>
2023-08-29 19:55:52 +0300

    Date, Time formatointia 😬

--- Utils/functions.js
+++ Utils/functions.js
@@ -1,9 +1,9 @@
 const doFetch = async (url, options = {}) => {
-  const response = await fetch(url, options)
-  if(!response.ok) {
-    throw new Error('Request failed!')
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error('Request failed!');
   }
-  const json = await response.json()
-  return json
-}
-export {doFetch};
+  const json = await response.json();
+  return json;
+};
+export { doFetch };
--- views/Single.js
+++ views/Single.js
@@ -4,19 +4,28 @@ import styles from '../components/style';
 import { mediaUrl } from '../Utils/app-config';
 
 const Single = ({ route, navigation }) => {
-  const { title, filename, description, user_id, time_added } = route.params;
+  const {
+    title,
+    filename,
+    description,
+    user_id,
+    time_added = new Date(),
+  } = route.params;
+
+  const formattedDate = new Date(time_added).toLocaleString('en-US', {
+    hour12: false,
+  });
+
   return (
     <SafeAreaView style={styles.Scontainer}>
       <Text style={styles.Stitle}>{title}</Text>
-      <Image
-        style={styles.Simage}
-        source={{ uri: mediaUrl + filename }}
-      />
+      <Image style={styles.Simage} source={{ uri: mediaUrl + filename }} />
       <Text style={styles.Stext}>{description}</Text>
       <Text style={styles.Stext}>{user_id}</Text>
-      <Text style={styles.Stext}>{time_added}</Text>
+      <Text style={styles.Stext}>{formattedDate}</Text>
     </SafeAreaView>
   );
 };
+
 
 export default Single;
